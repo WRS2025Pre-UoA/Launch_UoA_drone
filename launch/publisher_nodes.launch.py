564dfaad1a6da2bee8986d30ea69199b2b271f63
suzakutakumi <suzakutakumi3@gmail.com>
2024-09-28 19:26:27 +0900

    実装

---
+++ launch/publisher_nodes.launch.py
@@ -0,0 +1,69 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    # launchの構成を示すLaunchDescription型の変数の定義
+    ld = LaunchDescription()
+
+    namespace = "drone"
+
+    nodes = [
+        Node(
+            package='pressure',
+            executable='pressure_subscriber',
+            name='pressure',
+            namespace=namespace,
+            remappings=[('image_topic', 'pressure_image')]
+        ),
+
+        Node(
+            package='scan_qr',
+            executable='listener',
+            name='listener',
+            namespace=namespace,
+            remappings=[
+                ('image_raw', 'qr_image'),
+                ('qr_image', 'qr_result_image'),
+                ("qr_value", "qr_result_value")
+            ]
+        ),
+
+        Node(
+            package='rust',
+            executable='rust_subscriber',
+            name='rust',
+            namespace=namespace,
+            remappings=[('image_topic', 'rust_image')]
+        ),
+
+        Node(
+            package='drone_distribute_image',
+            executable='drone_distribute_image',
+            name='drone_distribute_image',
+            namespace=namespace,
+        ),
+
+        Node(
+            package='crack',
+            executable='crack_subscriber',
+            name='crack',
+            namespace=namespace,
+            remappings=[('image_raw', 'crack_image')]
+        ),
+
+        Node(
+            package='digital_twin_client',
+            executable='digital_twin_client',
+            namespace=namespace,
+            remappings=[('send_image', 'send_topic')],
+            parameters=[{"host": "52.193.111.81", "robot_id": "18"}]
+        )
+    ]
+
+    # LaunchDescriptionに、起動したいノードを追加する
+    for node in nodes:
+        ld.add_action(node)
+
+    # launch構成を返すようにする
+    return ld
